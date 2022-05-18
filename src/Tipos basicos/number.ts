356849456b54b55d74834c1ff401c353ef235144
Joao Carlos Sousa do Vale <joaocarlos.sousadovale@tipico.com>
2022-05-18 09:09:05 +0200

    initial push

---
+++ src/Tipos basicos/number.ts
@@ -0,0 +1,10 @@
+let idade: number;
+
+idade = 25;
+
+function somar(a: number, b: number) {
+    return a + b;
+}
+
+somar(1,1);
+somar(2,4);
---
+++ src/Tipos basicos/string.ts
@@ -0,0 +1,9 @@
+let nome: string;
+
+nome = 'João';
+
+function nomeCompleto(nome: string, sobrenome: string) {
+  return nome + ' ' + sobrenome;
+}
+
+nomeCompleto('João', 'Silva');
